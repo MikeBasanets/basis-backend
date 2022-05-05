1c7e52261fa93e9c9d7220c186dbfb463efc0ce1
Mike Basanets <mike.basanets@gmail.com>
2022-05-05 06:25:22 +0300

    Add cost filter, rename binary

--- algorithm/outfitCalculator.go
+++ algorithm/outfitCalculator.go
@@ -3,35 +3,64 @@ package algorithm
 import (
 	"basis/db"
 	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
-func makeAnyWardrobe() (db.Wardrobe, error) {
-	wardrobe, err := LoadClothing()
+func CalculateWardrobe(q db.QuizData) (db.Wardrobe, error) {
+	if !(q.Age >= 0 && q.Age < 200 && len(q.Designation) >= 3 && len(q.Designation) <= 15) {
+		return db.Wardrobe{}, errors.New("Wrong parameters")
+	}
+	clothing, err := LoadClothing()
 	if err != nil {
 		return db.Wardrobe{}, err
 	}
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(wardrobe.Pants), func(i, j int) {
-		wardrobe.Pants[i], wardrobe.Pants[j] = wardrobe.Pants[j], wardrobe.Pants[i]
-	})
-	rand.Shuffle(len(wardrobe.Shirts), func(i, j int) {
-		wardrobe.Shirts[i], wardrobe.Shirts[j] = wardrobe.Shirts[j], wardrobe.Shirts[i]
-	})
-	rand.Shuffle(len(wardrobe.Outerwear), func(i, j int) {
-		wardrobe.Outerwear[i], wardrobe.Outerwear[j] = wardrobe.Outerwear[j], wardrobe.Outerwear[i]
-	})
-	wardrobe.Pants = wardrobe.Pants[0 : 2+rand.Intn(3)]
-	wardrobe.Shirts = wardrobe.Shirts[0 : 3+rand.Intn(3)]
-	wardrobe.Outerwear = wardrobe.Outerwear[0 : 2+rand.Intn(3)]
-	return wardrobe, nil
+	budgetLeft := q.Budget
+	if budgetLeft < 1000 {
+		budgetLeft = 1000
+	}
+	clothing = filterCost(clothing, budgetLeft)
+	fmt.Println(len(clothing.Outerwear))
+	fmt.Println(len(clothing.Pants))
+	fmt.Println(len(clothing.Shirts))
+	clothing = filterRandomSubset(clothing)
+	return clothing, nil
 }
 
-func CalculateWardrobe(q db.QuizData) (db.Wardrobe, error) {
-	if q.Age >= 0 && q.Age < 255 && len(q.Designation) >= 3 && len(q.Designation) <= 15 {
-		return makeAnyWardrobe()
-	} else {
-		return db.Wardrobe{}, errors.New("Wrong parameters")
+func filterCost(w db.Wardrobe, budget int) db.Wardrobe {
+	result := db.Wardrobe{}
+	for _, v := range w.Outerwear {
+		if v.Price <= budget/5 {
+			result.Outerwear = append(result.Outerwear, v)
+		}
 	}
+	for _, v := range w.Pants {
+		if v.Price <= budget/10 {
+			result.Pants = append(result.Pants, v)
+		}
+	}
+	for _, v := range w.Shirts {
+		if v.Price <= budget/14 {
+			result.Shirts = append(result.Shirts, v)
+		}
+	}
+	return result
+}
+
+func filterRandomSubset(w db.Wardrobe) db.Wardrobe {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(w.Pants), func(i, j int) {
+		w.Pants[i], w.Pants[j] = w.Pants[j], w.Pants[i]
+	})
+	rand.Shuffle(len(w.Shirts), func(i, j int) {
+		w.Shirts[i], w.Shirts[j] = w.Shirts[j], w.Shirts[i]
+	})
+	rand.Shuffle(len(w.Outerwear), func(i, j int) {
+		w.Outerwear[i], w.Outerwear[j] = w.Outerwear[j], w.Outerwear[i]
+	})
+	w.Pants = w.Pants[0 : 2+rand.Intn(3)]
+	w.Shirts = w.Shirts[0 : 3+rand.Intn(3)]
+	w.Outerwear = w.Outerwear[0 : 2+rand.Intn(3)]
+	return w
 }
